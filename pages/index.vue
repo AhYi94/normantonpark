5853b256a2f93cf7572ca2e63c36504221216a2d
Adam <lingyunggie@gmail.com>
2021-06-30 21:52:39 +0800

    nuxtjs base

---
+++ pages/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <NuxtLink to="/about">
+      About (internal link that belongs to the Nuxt App)
+    </NuxtLink>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
