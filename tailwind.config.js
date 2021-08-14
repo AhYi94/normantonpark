3b7db3e4c1568025b1ff336646b6b07cb44fefdb
Adam <lingyunggie@gmail.com>
2021-08-14 18:15:58 +0800

    package typography

--- tailwind.config.js
+++ tailwind.config.js
@@ -35,5 +35,8 @@ module.exports = {
   variants: {
     extend: {},
   },
-  plugins: [require('@tailwindcss/aspect-ratio')],
+  plugins: [
+    require('@tailwindcss/aspect-ratio'),
+    require('@tailwindcss/typography'),
+  ],
 }
